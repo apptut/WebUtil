384227bffb1c780047648f3f40e34a3d98fc1aed
liangqi <liangqi000@gmail.com>
2016-08-31 20:10:03 +0800

    init project

---
+++ src/RSAUtil.php
@@ -0,0 +1,90 @@
+<?php
+namespace WebUtil;
+
+/**
+ * ------------------
+ *   rsa加密解密
+ * ------------------
+ *
+ * User: liangqi
+ * Date: 16/8/31
+ * Time: 下午7:07
+ */
+class RSAUtil
+{
+    /**
+     * 公钥加密
+     *
+     * @param $content
+     * @param $private_key
+     * @return null|string
+     */
+    public static function encodeByPrivate($content, $private_key)
+    {
+        //转换为openssl密钥
+        $res = self::formatPriKey(openssl_get_privatekey($private_key));
+        $encrypted = '';
+        $rel = openssl_private_encrypt($content, $encrypted, $res);
+        if ($rel) {
+            return base64_encode($encrypted);
+        }
+        return null;
+    }
+
+    /**
+     * 私钥解密
+     *
+     * @param $content
+     * @param $public_key
+     * @return bool|null
+     */
+    public static function decodeByPublic($content, $public_key)
+    {
+        $res = self::formatPubKey($public_key);
+        $decoded = '';
+        $rel = openssl_public_decrypt(base64_decode($content), $decoded, $res);
+        if ($rel) {
+            return $rel;
+        }
+
+        return null;
+    }
+
+    /**
+     * 格式化公钥
+     *
+     * @param $pubKey
+     * @return string
+     */
+    private static function formatPubKey($pubKey)
+    {
+        $fKey = "-----BEGIN PUBLIC KEY-----\n";
+        $len = strlen($pubKey);
+        for ($i = 0; $i < $len;) {
+            $fKey = $fKey . substr($pubKey, $i, 64) . "\n";
+            $i += 64;
+        }
+        $fKey .= "-----END PUBLIC KEY-----";
+        return $fKey;
+    }
+
+
+    /**
+     * 格式化私钥
+     *
+     * $priKey PKCS#1格式的私钥串
+     * return pem格式私钥， 可以保存为.pem文件
+     */
+    private static function formatPriKey($priKey)
+    {
+        $fKey = "-----BEGIN RSA PRIVATE KEY-----\n";
+        $len = strlen($priKey);
+        for ($i = 0; $i < $len;) {
+            $fKey = $fKey . substr($priKey, $i, 64) . "\n";
+            $i += 64;
+        }
+        $fKey .= "-----END RSA PRIVATE KEY-----";
+        return $fKey;
+    }
+
+}
